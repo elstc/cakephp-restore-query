5131ab4d8bbdb1a73b608adb9f823fe54dd0fae8
nojimage <nojimage@gmail.com>
2018-04-18 17:42:51 +0900

    add travis.yaml

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,8 +3,6 @@
 use Cake\Core\Plugin;
 
 /**
- * Test suite bootstrap for LoginAttempts.
- *
  * This function is used to find the location of CakePHP whether CakePHP
  * has been installed as a dependency of the plugin, or the plugin is itself
  * installed as a dependency of an application.
